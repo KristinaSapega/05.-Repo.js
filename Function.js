eab313e2a8eec31c60652ac0ee14db6b00c52928
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2023-04-12 13:36:13 +0700

    1.2

--- Function.js
+++ Function.js
@@ -8,3 +8,14 @@ const puzzle = (a, b) => {
   };
   
   console.log(puzzle(8, 4));
+
+  //1.2
+  const puzzle = (a, b) => {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  };
+  
+  console.log(puzzle(6, 6));
