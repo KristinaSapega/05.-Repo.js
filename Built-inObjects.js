7ec9928ead32f8a5788dc7846e4ddd105c978b6a
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2023-04-19 12:27:18 +0700

    2

--- Built-inObjects.js
+++ Built-inObjects.js
@@ -3,3 +3,13 @@ let str = 'js';
 console.log(str.toUpperCase()); //JS
 
 //2
+const fruits = ['apple', 'pineapple', 'pear', 'strawberry', 'blueberry'];
+const search = 'apple';
+fruits.forEach((fruits) => {
+    if (fruits.toLocaleLowerCase ().includes(search.toLowerCase ())) {
+        console.log(fruits);
+    }
+}
+) // apple pineapple
+
+//3
