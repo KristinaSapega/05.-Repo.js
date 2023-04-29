5e064fd1c049ec2d8184ed6e50b259c26581acf6
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2023-04-29 23:12:12 +0700

    2

--- Exam.js
+++ Exam.js
@@ -29,6 +29,27 @@ function strCount(str, letter){
   console.log (strCount ("sun shine is bright", "m"));
 
   //task 2
+// Создаем функцию squareSum, которая принимает массив чисел numbers в качестве аргумента
+    function squareSum(numbers) {
+// Используем метод map для получения массива квадратов элементов
+    return numbers.map(function(x) {
+      return x * x;
+    })
+// Используем метод reduce для получения суммы элементов массива
+    .reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+  }
   
+  console.log(squareSum([3, 2, 2]));
+
+  //вар 2
+  function squareSum(numbers){
+    return numbers.reduce((sum,num) => sum + (num * num), 0);
+  }
+  
+  console.log(squareSum([3, 2, 2]));
 
 
+
+  //task 3
